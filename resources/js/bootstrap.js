344cbe39574f9b6a958db49bc78860507de76289
engr-amanshah <amanshah12sweer@gmail.com>
2026-06-23 14:33:01 +0500

    added

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -2,3 +2,12 @@ import axios from 'axios';
 window.axios = axios;
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
+const tokenMeta = typeof document !== 'undefined' && document.querySelector('meta[name="csrf-token"]');
+const csrfToken = tokenMeta ? tokenMeta.getAttribute('content') : null;
+if (csrfToken) {
+	window.axios.defaults.headers.common['X-CSRF-TOKEN'] = csrfToken;
+	// Make token available to other libs that might inspect `window.Laravel`
+	window.Laravel = window.Laravel || {};
+	window.Laravel.csrfToken = csrfToken;
+}
